c4b26b811ff3ae62a607da747a352f67bde73c02
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2021-06-29 13:16:32 +0200

    fix(dependencies): change to import {program} from 'commander'

--- feature-runner/run-features.ts
+++ feature-runner/run-features.ts
@@ -5,7 +5,7 @@ import {
 	restStepRunners,
 	storageStepRunners,
 } from '@nordicsemiconductor/e2e-bdd-test-runner'
-import * as program from 'commander'
+import { program } from 'commander'
 import * as chalk from 'chalk'
 import * as path from 'path'
 import { randomEmail } from './lib/randomEmail'
