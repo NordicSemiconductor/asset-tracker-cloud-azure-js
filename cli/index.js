d1cbd3e0496928b7057e5415fb22220f7018c5bd
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-09-02 11:04:41 +0200

    style: reformat

--- cli/index.js
+++ cli/index.js
@@ -1,12 +1,12 @@
 const die = (err, origin) => {
-  console.error(`An unhandled exception occured!`);
-  console.error(`Exception origin: ${JSON.stringify(origin)}`);
-  console.error(err);
-  if (process.env.DONT_DIE_ON_UNHANDLED_EXCEPTIONS === undefined)
-    process.exit(1);
-};
+	console.error(`An unhandled exception occured!`)
+	console.error(`Exception origin: ${JSON.stringify(origin)}`)
+	console.error(err)
+	if (process.env.DONT_DIE_ON_UNHANDLED_EXCEPTIONS === undefined)
+		process.exit(1)
+}
 
-process.on("uncaughtException", die);
-process.on("unhandledRejection", die);
+process.on('uncaughtException', die)
+process.on('unhandledRejection', die)
 
-import("../dist/cli/cli.js");
+import('../dist/cli/cli.js')
