15e20eac128176b62ceffc91f82ae84a7f09f6a6
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-09-16 16:20:27 +0200

    fix(pack): dependencies were not working from .mjs files

--- geolocateCellFromUnwiredLabs/handler.mjs
+++ geolocateCellFromUnwiredLabs/handler.mjs
@@ -1,2 +1,2 @@
-import handler from '../dist/geolocateCellFromUnwiredLabs/geolocateCellFromUnwiredLabs.js'
-export default handler
+import handler from "../dist/geolocateCellFromUnwiredLabs/geolocateCell.js";
+export default handler;
