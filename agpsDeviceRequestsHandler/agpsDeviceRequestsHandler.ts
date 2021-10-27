80517040f69a793133b9c9cf722d5099f836d6cd
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-10-27 15:04:16 +0200

    feat(agps): remove network mode from A-GPS implementation
    Right now the network mode is not needed to
    request A-GPS data, so this is removed to
    make the implemenation leaner and faster

--- agpsDeviceRequestsHandler/agpsDeviceRequestsHandler.ts
+++ agpsDeviceRequestsHandler/agpsDeviceRequestsHandler.ts
@@ -5,7 +5,6 @@ import { agpsRequestSchema } from '../agps/types.js'
 import { isRight } from 'fp-ts/lib/Either.js'
 import { Static } from '@sinclair/typebox'
 import { fromEnv } from '../lib/fromEnv.js'
-import iothub from 'azure-iothub'
 import {
 	QueueServiceClient,
 	QueueClient,
@@ -16,16 +15,12 @@ const validateAgpsRequest = validateWithJSONSchema(agpsRequestSchema)
 
 const config = () =>
 	fromEnv({
-		iotHubConnectionString: 'IOTHUB_CONNECTION_STRING',
 		storageAccountName: 'STORAGE_ACCOUNT_NAME',
 		storageAccessKey: 'STORAGE_ACCESS_KEY',
 		agpsRequestsQueueName: 'AGPS_REQUESTS_QUEUE_NAME',
 	})({
 		...process.env,
 	})
-
-// Local cache for network mode of devices
-const nwReported: Record<string, string> = {}
 
 /**
  * Queue A-GPS requests from devices
@@ -52,20 +47,12 @@ const agpsDeviceRequestsHandler: AzureFunction = async (
 	log(context)({ context, requests })
 
 	const timestamp = new Date()
-	let iotHubRegistry: iothub.Registry
 	let queueClient: QueueClient
 
 	try {
-		const {
-			storageAccountName,
-			storageAccessKey,
-			iotHubConnectionString,
-			agpsRequestsQueueName,
-		} = config()
+		const { storageAccountName, storageAccessKey, agpsRequestsQueueName } =
+			config()
 
-		iotHubRegistry = iothub.Registry.fromConnectionString(
-			iotHubConnectionString,
-		)
 		queueClient = new QueueServiceClient(
 			`https://${storageAccountName}.queue.core.windows.net`,
 			new StorageSharedKeyCredential(storageAccountName, storageAccessKey),
@@ -74,18 +61,6 @@ const agpsDeviceRequestsHandler: AzureFunction = async (
 	} catch (error) {
 		logError(context)({ error: (error as Error).message })
 		return
-	}
-
-	// Since this function receives all device message we can use reported updates
-	// directly to cache the network property here
-	for (let i = 0; i < requests.length; i++) {
-		const nw = (requests as Record<string, any>[])[i]?.properties?.reported?.dev
-			?.v?.nw
-		if (nw !== undefined) {
-			const deviceId = context.bindingData.propertiesArray[i].deviceId
-			nwReported[deviceId] = nw
-			log(context)(`${deviceId}: ${nw}`)
-		}
 	}
 
 	// Find A-GPS requests
@@ -110,36 +85,13 @@ const agpsDeviceRequestsHandler: AzureFunction = async (
 
 	log(context)({ agpsRequests })
 
-	// Fetch reported network for the devices
-	const nwReportedUpdates = (
-		await iotHubRegistry
-			.createQuery(
-				`SELECT deviceId, properties.reported.dev.v.nw FROM devices WHERE deviceId IN [${[
-					...new Set(agpsRequests.map(({ deviceId }) => deviceId)), // Ensure deviceIds are unique
-				]
-					.map((deviceId) => `'${deviceId}'`)
-					.join(',')}]`,
-			)
-			.nextAsTwin()
-	).result as unknown as { deviceId: string; nw: string }[]
-	// Update our local cache
-	nwReportedUpdates.forEach(({ deviceId, nw }) => {
-		nwReported[deviceId] = nw
-		log(context)(`${deviceId}: ${nw}`)
-	})
-
-	log(context)({ nwReported })
-
 	// Build list of valid requests
 	const deviceRequests: {
 		request: Static<typeof agpsRequestSchema>
 		deviceId: string
 	}[] = []
 	agpsRequests.forEach(({ request, deviceId }) => {
-		const valid = validateAgpsRequest({
-			...request,
-			nw: nwReported[deviceId],
-		})
+		const valid = validateAgpsRequest(request)
 		if (isRight(valid)) {
 			deviceRequests.push({
 				request: valid.right,
