eab39425a55ff3584f33ae03bcebf7fc597a8fc8
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-05-25 13:01:57 +0200

    refactor: use API client

--- cli/iot/ioTHubDPSInfo.ts
+++ cli/iot/ioTHubDPSInfo.ts
@@ -1,6 +1,6 @@
-import { AccessToken } from '@azure/core-auth'
+import { IotDpsClient } from '@azure/arm-deviceprovisioningservices'
+import { IotHubClient } from '@azure/arm-iothub'
 import { AzureCliCredential } from '@azure/identity'
-import fetch from 'node-fetch'
 
 export const globalIotHubDPSHostname = 'global.azure-devices-provisioning.net'
 
@@ -26,55 +26,43 @@ export const ioTHubDPSInfo =
 	}> => {
 		const creds =
 			typeof credentials === 'function' ? await credentials() : credentials
-		const token = (await creds.credentials.getToken([])) as AccessToken
-		const subscriptionId = creds.subscriptionId
 
-		return Promise.all([
-			fetch(
-				`https://management.azure.com/subscriptions/${subscriptionId}/resourceGroups/${resourceGroupName}/providers/Microsoft.Devices/provisioningServices/${resourceGroupName}ProvisioningService/listkeys?api-version=2018-01-22`,
-				{
-					method: 'POST',
-					headers: {
-						Authorization: `Bearer ${token.token}`,
-						'Content-type': `application/json`,
-					},
-				},
+		const armIotDpsClient = new IotDpsClient(
+			creds.credentials,
+			creds.subscriptionId,
+			{
+				apiVersion: '2021-10-15',
+			},
+		)
+		const armIotHubClient = new IotHubClient(
+			creds.credentials,
+			creds.subscriptionId,
+		)
+
+		const [keys, dpsInfo, iotHubInfo] = await Promise.all([
+			armIotDpsClient.iotDpsResource.listKeys(
+				`${resourceGroupName}ProvisioningService`,
+				resourceGroupName,
 			),
-			fetch(
-				`https://management.azure.com/subscriptions/${subscriptionId}/resourceGroups/${resourceGroupName}/providers/Microsoft.Devices/provisioningServices/${resourceGroupName}ProvisioningService?api-version=2018-01-22`,
-				{
-					headers: {
-						Authorization: `Bearer ${token.token}`,
-						'Content-type': `application/json`,
-					},
-				},
+			armIotDpsClient.iotDpsResource.get(
+				`${resourceGroupName}ProvisioningService`,
+				resourceGroupName,
 			),
-			fetch(
-				`https://management.azure.com/subscriptions/${subscriptionId}/resourceGroups/${resourceGroupName}/providers/Microsoft.Devices/IoTHubs/${resourceGroupName}IoTHub?api-version=2018-01-22`,
-				{
-					headers: {
-						Authorization: `Bearer ${token.token}`,
-						'Content-type': `application/json`,
-					},
-				},
+			armIotHubClient.iotHubResource.get(
+				resourceGroupName,
+				`${resourceGroupName}IoTHub`,
 			),
 		])
-			.then(
-				async (res) =>
-					Promise.all(res.map(async (r) => r.json())) as Promise<any[]>,
-			)
-			.then(
-				([
-					{ value },
-					{
-						properties: { serviceOperationsHostName },
-					},
-					{
-						properties: { hostName },
-					},
-				]) => ({
-					hostname: hostName as string,
-					connectionString: `HostName=${serviceOperationsHostName};SharedAccessKeyName=provisioningserviceowner;SharedAccessKey=${value[0].primaryKey}`,
-				}),
-			)
+
+		let primaryKey: string | undefined = undefined
+
+		for await (const key of keys) {
+			primaryKey = key.primaryKey
+			break
+		}
+
+		return {
+			hostname: iotHubInfo.properties?.hostName as string,
+			connectionString: `HostName=${dpsInfo.properties.serviceOperationsHostName};SharedAccessKeyName=provisioningserviceowner;SharedAccessKey=${primaryKey}`,
+		}
 	}
