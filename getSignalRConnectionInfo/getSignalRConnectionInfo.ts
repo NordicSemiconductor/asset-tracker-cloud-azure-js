9abd591187f18d09024f406d73b949ee962a580b
Pisut Sritrakulchai <pudkrong@hotmail.com>
2023-10-26 14:50:01 +0200

    feat: migrate signalR functions

--- getSignalRConnectionInfo/getSignalRConnectionInfo.ts
+++ getSignalRConnectionInfo/getSignalRConnectionInfo.ts
@@ -1,15 +1,14 @@
-import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import type { FunctionInput, HttpHandler } from '@azure/functions'
 import { result } from '../lib/http.js'
 
-const getSignalRConnectionInfo: AzureFunction = async (
-	context: Context,
-	_: HttpRequest,
-	connectionInfo: {
-		url: string
-		accessToken: string
-	},
-): Promise<void> => {
-	context.res = result(context)(connectionInfo)
-}
+const getSignalRConnectionInfo =
+	(signalRConnectionInfo: FunctionInput): HttpHandler =>
+	async (_, context) => {
+		const connectionInfo = context.extraInputs.get(signalRConnectionInfo) as {
+			url: string
+			accessToken: string
+		}
+		return result(context)(connectionInfo)
+	}
 
 export default getSignalRConnectionInfo
